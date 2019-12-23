7746de65953257613e2937985c411046eb985458
pangzhenyu <1484719918@qq.com>
2019-12-23 20:56:41 +0800

    test

--- V17-web/v17-sso/src/main/java/com/qianfeng/v17sso/controller/SSOController.java
+++ V17-web/v17-sso/src/main/java/com/qianfeng/v17sso/controller/SSOController.java
@@ -25,6 +25,10 @@ import java.util.Map;
 @RequestMapping("sso")
 public class SSOController {
 
+    public static void main(String[] args){
+        System.out.println();
+    }
+
     @Reference
     private IUserService userService;
 
